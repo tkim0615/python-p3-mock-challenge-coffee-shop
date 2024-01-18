e40e35d951d91222db31fe342e23e1d6f15c42ee
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-18 11:03:56 -0500

    mock

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,86 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) >= 3 and not hasattr(self, 'name'):
+            self._name = name
+        # else:
+        #     raise Exception('invalid name/ must be 3 characters or longer. Can\'t change name')
+    
         
     def orders(self):
-        pass
-    
+        return [order for order in Order.all if order.coffee == self]
+        
     def customers(self):
-        pass
+        return list(set([order.customer for order in self.orders()]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        price_list = [order.price for order in self.orders()]
+        return round((sum(price_list) / len(price_list)), 2)
+    
 
 class Customer:
+    all = []
     def __init__(self, name):
         self.name = name
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1<= len(name) <=15:
+            self._name = name
+    
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list(set([order.coffee for order in self.orders()]))
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
+
     
 class Order:
+    all = []
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+        
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, price):
+        if isinstance(price, float) and 1.0 <= price <= 10.0 and not hasattr(self, 'price'):
+            self._price = price
+        # else:
+        #     raise Exception('price must be between 1 and 10. Can\'t change price')
+
+    @property
+    def customer(self):
+        return self._customer
+    @customer.setter
+    def customer(self, customer):
+        if isinstance(customer, Customer):
+            self._customer = customer
+    @property
+    def coffee(self):
+        return self._coffee
+    @coffee.setter
+    def coffee(self, coffee):
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,34 @@ from classes.many_to_many import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
+    c1 = Customer('s')
+    c2 = Customer('Henry')
+    c3 = Customer('Ada')
+    c4 = Customer('michael')
+
+    cf1 = Coffee('espresso')
+    cf2 = Coffee('cold brew')
+    cf3 = Coffee('flat white')
+    cf4 = Coffee('cappuccino')
+    
+    order1 = Order(c1, cf1, 3.99)
+    order2 = Order(c1, cf2, 2.99)
+    order3 = Order(c1, cf1, 2.50)
+
+    order4= Order(c1, cf2, 5.29)
+    order5= Order(c2, cf1, 1.95)
+
+    order6= Order(c2, cf2, 1.59)
+
+
+
+
+
+
+
+
+
+
+
+
     ipdb.set_trace()
